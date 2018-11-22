56dc70ff48ed7b2c0f3722a26d57a5e423f0a337
jaswant-rana <jaswant@ucreate.co.in>
2018-11-22 14:58:33 +0530

    change text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Chaos Monkey
                 </div>
 
                 <div class="links">
